9062cafad33c3c62678dedc15f2fd69ab4235418
Grace H <gracefh@mit.edu>
2022-11-16 16:43:31 -0500

    set up github pages

--- classic/docusaurus.config.js
+++ classic/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Bluefish (Alpha Release)',
   tagline: 'Create beautiful, customizable charts and diagrams in React.',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://bluefishjs.org',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -17,8 +17,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'joshpoll', // Usually your GitHub org/user name.
+  projectName: 'bluefish-docs', // Usually your repo name.
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
