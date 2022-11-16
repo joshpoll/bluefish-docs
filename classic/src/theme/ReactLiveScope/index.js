e01e6b5c5a50a906b921f63484dcd07fbd2895d0
Josh Pollock <jopo@mit.edu>
2022-11-16 17:55:13 -0500

    fix server build

--- classic/src/theme/ReactLiveScope/index.js
+++ classic/src/theme/ReactLiveScope/index.js
@@ -1,21 +1,23 @@
 import React from "react";
-import { Rect, SVG, Align, Text, Ref, Group, Row, Col, Connector } from "@bluefish-js/core";
+// import * as Bluefish from "@bluefish-js/core";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+const Bluefish = ExecutionEnvironment.canUseDOM ? require("@bluefish-js/core") : null;
 
 const Node = React.forwardRef(function _Node({ name, value }, ref) {
   return (
-    <Group name={name} ref={ref}>
-      <Text name={name + '-value'} contents={value} fontSize={'30px'} />
-      <Rect name={name + '-background'} width={50} height={65} rx={5} fill={'none'} stroke={'cornflowerblue'} />
-      <Text name={name + '-label'} contents={name} fontSize={'12px'} />
-      <Align center>
-        <Ref to={name + '-value'} />
-        <Ref to={name + '-background'} />
-      </Align>
-      <Align center to={'topCenter'}>
-        <Ref to={name + '-label'} />
-        <Ref to={name + '-background'} />
-      </Align>
-    </Group>
+    <Bluefish.Group name={name} ref={ref}>
+      <Bluefish.Text name={name + '-value'} contents={value} fontSize={'30px'} />
+      <Bluefish.Rect name={name + '-background'} width={50} height={65} rx={5} fill={'none'} stroke={'cornflowerblue'} />
+      <Bluefish.Text name={name + '-label'} contents={name} fontSize={'12px'} />
+      <Bluefish.Align center>
+        <Bluefish.Ref to={name + '-value'} />
+        <Bluefish.Ref to={name + '-background'} />
+      </Bluefish.Align>
+      <Bluefish.Align center to={'topCenter'}>
+        <Bluefish.Ref to={name + '-label'} />
+        <Bluefish.Ref to={name + '-background'} />
+      </Bluefish.Align>
+    </Bluefish.Group>
   );
 });
 
@@ -23,15 +25,7 @@ const Node = React.forwardRef(function _Node({ name, value }, ref) {
 const ReactLiveScope = {
   React,
   ...React,
-  Rect,
-  SVG,
-  Align,
-  Text,
-  Ref,
-  Group,
-  Row,
-  Col,
+  ...Bluefish,
   Node,
-  Connector,
 };
 export default ReactLiveScope;
